e01f3d7e1cc98ee9492ae0c19c3e2a6f16e4518f
jhonsaul1234 <mamanicruzjhon42@gmail.com>
2026-04-19 22:31:07 -0500

    Configurar Firebase en frontend

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDVucsbdxcIWpLOzqntRE427SlREOT1eNE",
+  authDomain: "perdon-app.firebaseapp.com",
+  projectId: "perdon-app",
+  storageBucket: "perdon-app.firebasestorage.app",
+  messagingSenderId: "95338602793",
+  appId: "1:95338602793:web:7fb6116cf91dbc5cd71738"
+};
